680bc69d5d5a212751afa1170ea61ba9212dbc4a
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-27 13:52:34 -0700

    Fix for https://github.com/runkitdev/parallel-branch/issues/4, although it could be better. Also fix the strange behavior from treating Function declarations as unary expressions.
    Reviewed by @tolmasky.

--- parallel-branch-babel-plugin/parser-plugin.js
+++ parallel-branch-babel-plugin/parser-plugin.js
@@ -5,75 +5,142 @@ module.exports = parser `parallel-branch` (({ types: t }, superclass) =>
 
 class ParallelBranchParser extends superclass
 {
-    // By making parallel a unary operator, it opens us up to weird newline
-    // insertion bugs. parallel function f() { }() shouldn't parse but does.
-    parseStatementContent(...args)
+    // We want to extend ScopeHandler to understand "parallel" scopes much like
+    // "async" scopes. The easiest way to do this is to create a
+    // `nextScopeIsParallel` property and override enter() to mark the scope as
+    // parallel if it's true. This allows us to avoid having to edit every
+    // Function Node creation method to also understand parallel scopes.
+    getScopeHandler()
     {
-        const statement = super.parseStatementContent(...args);
+        return class ParallelScopeHandler extends super.getScopeHandler()
+        {
+            enter(...args)
+            {
+                super.enter(...args);
 
-        if (statement.type !== "ExpressionStatement")
-            return statement;
+                if (this.nextScopeIsParallel)
+                    this.currentVarScope().inParallel = true;
 
-        const { expression } = statement;
+                this.nextScopeIsParallel = false;
+            }
 
-        if (expression.type !== "FunctionExpression" || !expression.parallel)
-            return statement;
-
-        if (!expression.id)
-            this.raise(this.state.start,
-                `Parallel Function Declarations need a name.`);
-
-        return { ...expression, type: "FunctionDeclaration" };
+            inParallel()
+            {
+                return this.currentVarScope().inParallel
+            }
+        }
     }
 
-    parseBranchExpression(...args)
+    parseFunction(...args)
     {
-        this.next();
+        // We *have to* check this first.
+        const wasInParallel = this.scope.nextScopeIsParallel;
+        const node = super.parseFunction(...args);
 
-        const argument = this.parseMaybeUnary(...args);
-
-        if (argument.type !== "CallExpression")
-            this.raise(this.state.start,
-                `Can only branch on a function call.`);
-
-        return t.CallExpression(t.Identifier("branch"), [argument]);
+        return wasInParallel ? Object.assign(node, { parallel: true }) : node;
     }
 
-    parseBranchingExpression(...args)
+    parseStatementContent(context, ...rest)
     {
+        // This is basically the same way async functions are detected in
+        //
+        // https://github.com/babel/babel/blob/master/packages/babel-parser/src/parser/statement.js#L268
+        //
+        // However, in Babel, this is done at the end if no other token matches.
+        // We can get away with checking whether we're at a parallel function
+        // *first* because "parallel" shouldn't collide with any other tokens.
+        if (!this.isParallelFunction())
+            return super.parseStatementContent(context, ...rest);
+
+        if (context)
+            this.unexpected(null,
+                `Parallel functions can only be declared at the top level or ` +
+                `inside a block`);
+
+        // At this point we know we either have a parallel function.
+        this.scope.nextScopeIsParallel = true;
         this.next();
 
-        const argument = this.parseMaybeUnary(...args);
+        return this.parseFunctionStatement(this.startNode(), false, !context);
+    }
 
-        return t.CallExpression(t.Identifier("branching"), [argument]);
+    isParallelFunction()
+    {
+        // We basically want identical behavior to isAsyncFunction, except with
+        // the "parallel" identifier:
+        //
+        // https://github.com/babel/babel/blob/master/packages/babel-parser/src/parser/statement.js#L1756
+        //
+        // One option would be to copy the contents of the function, and all of its
+        // un-exported dependencies, but that would require us tracking it indefinitely.
+        // Instead, we just modify the behavior of isContextual to check "parallel"
+        // it is asked to check "async". This is the only difference in the function.
+        const hasIsContextual = Object.hasOwnProperty("isContextual");
+        const trueIsContextual = this.isContextual;
+        this.isContextual = word => trueIsContextual
+            .call(this, word === "async" ? "parallel" : word);
+        const result = this.isAsyncFunction();
+
+        if (hasIsContextual)
+            this.isContextual = trueIsContextual;
+        else
+            delete this.isContextual;
+
+        return result;
+    }
+
+
+    checkReservedWord(word, startLoc, ...rest)// checkKeywords, isBinding)
+    {
+        if (this.scope.inParallel() && word === "branch")
+            this.raise(
+                startLoc,
+                "Can not use 'branch' as identifier inside a parallel function");
+
+        if (this.scope.inParallel() && word === "branching")
+            this.raise(
+                startLoc,
+                "Can not use 'branching' as identifier inside a parallel function");
+
+        return super.checkReservedWord(word, startLoc, ...rest);
     }
 
     parseMaybeUnary(...args)
     {
-        if (this.isContextual("branch"))
-            return this.parseBranchExpression(...args);
+        // To make our lives easier, we parse parallel function *expressions* as
+        // if they were the "parallel operator" applied to a function expression.
+        if (this.isContextual("parallel"))
+        {
+            this.scope.nextScopeIsParallel = true;
+            this.next();
 
-        if (this.isContextual("branching"))
-            return this.parseBranchingExpression(...args);
-
-        if (!this.isContextual("parallel"))
             return super.parseMaybeUnary(...args);
+        }
+
+        if (this.scope.inParallel() && this.isContextual("branch"))
+            return this.parseBranchExpression("branch");
+
+        if (this.scope.inParallel() && this.isContextual("branching"))
+            return this.parseBranchExpression("branching");
+
+        return super.parseMaybeUnary(...args);
+    }
+
+    parseBranchExpression(keyword)
+    {
+        const node = this.startNode();
+
+        if (this.state.inParameters)
+            this.raise(
+                node.start,
+                `${keyword} is not allowed in parallel function parameters`);
 
         this.next();
 
-        const argument = this.parseMaybeUnary();
+        node.callee = this.createIdentifier(this.startNodeAtNode(node), keyword);
+        node.arguments = [this.parseMaybeUnary()];
 
-        if (argument.type !== "FunctionExpression")
-            this.raise(this.state.start, `Only static secrets are allowed.`);
-
-        return Object.assign(argument, { parallel: true });
-    }
-
-    checkReservedWord(word, startLoc, ...rest)
-    {
-//        if (word === "parallel")
-//            this.raise(startLoc, `Unexpected keyword '${word}'`);
-
-        return super.checkReservedWord(word, startLoc, ...rest);
+        return this.finishNode(node, "CallExpression");
     }
 });
+
---
+++ parallel-branch-spawn/spawn.js
@@ -0,0 +1,43 @@
+const { data, number, string } = require("@algebraic/type");
+const { spawn: spawnAsync } = require("child_process");
+const { Readable } = require("stream");
+const { hasOwnProperty } = Object;
+
+const Result = data `spawn.Result` (
+    stdout      => string,
+    exitCode    => number );
+
+const ExitCodeError = data `spawn.ExitCodeError` (
+    exitCode    => number,
+    stderr      => string );
+
+module.exports = function spawn(command, args = [], options = { })
+{
+    return new Promise(function (resolve, reject)
+    {
+        const stdio = options.stdio || [];
+        const hasReadableStdInStream = stdio[0] instanceof Readable;
+        const modifiedOptions = hasReadableStdInStream ?
+            { ...options, stdio: ["pipe", ...stdio.slice(1)] } :
+            options;
+        const throwOnExitCode =
+            hasOwnProperty.call(options, "throwOnExitCode") ?
+            !!options.throwOnExitCode : true;
+
+        const process = spawnAsync(command, args, modifiedOptions);
+
+        if (hasReadableStdInStream)
+            stdio[0].pipe(process.stdin);
+
+        const output = { stdout: "", stderr: "" };
+
+        process.on("error", error => reject(error));
+        process.on("exit", exitCode =>
+            exitCode !== 0 && throwOnExitCode ?
+                reject(ExitCodeError({ ...output, exitCode })) :
+                resolve(Result({ ...output, exitCode })));
+
+        process.stdout.on("data", data => output.stdout += data);
+        process.stderr.on("data", data => output.stderr += data);
+    });
+}
