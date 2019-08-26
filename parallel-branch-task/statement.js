641601d118e1133be9b0b32ad30ce4a4195c7992
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-08-26 11:54:02 -0700

    First pass at statement.
    Reviewed by @tolmasky.

---
+++ parallel-branch-task/statement.js
@@ -0,0 +1,40 @@
+const { data, number, string } = require("@algebraic/type");
+const union = require("@algebraic/type/union-new");
+const DenseIntSet = Array;
+
+
+const Statement     =   data `Statement` (
+    address         =>  number,
+    dependencies    =>  DenseIntSet,
+    dependents      =>  DenseIntSet,
+    operation       =>  Operation,
+    block           =>  Function );
+
+module.exports      =   Statement;
+
+const Operation     =   union `Operation` (
+    is              =>  Operation.Step,
+    or              =>  Operation.Return,
+    or              =>  Operation.Branch );
+
+Operation.Step      =   data `Operation.Step` ();
+Operation.Return    =   data `Operation.Return` ();
+Operation.Branch    =   data `Operation.Branch` (
+    binding         =>  string );
+
+Statement.Operation =   Operation;
+
+Operation.deserialize = function (opcode, ...rest)
+{
+    return  opcode === 0 ? Operation.Step :
+            opcode === 1 ? Operation.Return :
+            Operation.Branch({ binding: rest[0] });
+}
+
+Statement.deserialize = function (serialized, address)
+{
+    const [dependencies, dependents, block, ...rest] = serialized;
+    const operation = Operation.deserialize(...rest);
+
+    return Statement({ address, dependencies, dependents, block, operation });
+}
